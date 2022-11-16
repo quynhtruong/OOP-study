d1c0b0b365d1a498472f970a0ac7b7054c869bc3
trangdo144 <117735432+trangdo144@users.noreply.github.com>
2022-11-16 23:05:54 +1100

    Trang code lesson 3

---
+++ Lesson3/Dog.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+public class Dog {
+    void bark(int numOfBark){
+        while (numOfBark > 0){
+            System.out.println("ruff");
+            numOfBark--;
+        }
+    }
+}
---
+++ Lesson3/DogTestDrive.java
@@ -0,0 +1,12 @@
+package Lesson3;
+
+public class DogTestDrive {
+    public static void main(String[] args){
+        Dog d = new Dog();
+        int numOfBark = 3;
+        System.out.println(numOfBark);
+        d.bark(numOfBark);
+        System.out.println(numOfBark);
+    }
+
+}
